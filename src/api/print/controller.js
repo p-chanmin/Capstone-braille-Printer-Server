98cf27cbc1e23c2c4f9c80f803249816bc9ed406
p-chanmin <pc_m111@naver.com>
2023-03-11 21:10:12 +0900

    #9 파일 경로 변경

--- src/api/print/controller.js
+++ src/api/print/controller.js
@@ -102,7 +102,7 @@ exports.getBrailleData = async (ctx, next) => {
 
     console.log(path.join(__dirname, '../../braille/braillePrint.py'));
 
-    const pythonProcess = spawn('python', [path.join(__dirname, '../../braille/braillePrint.py'), content]);
+    const pythonProcess = spawn('python', ['../../braille/braillePrint.py', content]);
 
     let result = "";
 
